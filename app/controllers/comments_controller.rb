1f318601193483a87dee0dbe80ef0a3f24ba828c
Josy <josyanjos.souza@gmail.com>
2019-04-08 20:45:16 -0300

    Implement comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,9 +7,6 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
-		@comment.destroy
-		redirect_to post_path(@post)
+
 	end
 end
